bfcef8608b5c043d15cecb7090d9ca40dd6d9b9f
skydogcat23 <skydogcat23@gmail.com>
2020-08-24 14:23:30 +0530

    Updates

---
+++ precache-manifest.1726c38a3c6e9e0269a1d2194e899ab6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52793f09df7ad0ea906b24af30b9cb58",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "9475ac3e9623ae249203",
+    "url": "/pomodoro-clock/static/css/main.9cb741d3.chunk.css"
+  },
+  {
+    "revision": "dc027bb5d5d81408afdd",
+    "url": "/pomodoro-clock/static/js/2.9c55ae24.chunk.js"
+  },
+  {
+    "revision": "d2907550b0749ad10542a424f45133ae",
+    "url": "/pomodoro-clock/static/js/2.9c55ae24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9475ac3e9623ae249203",
+    "url": "/pomodoro-clock/static/js/main.f19cced2.chunk.js"
+  },
+  {
+    "revision": "d420e891230cef74bfe7",
+    "url": "/pomodoro-clock/static/js/runtime-main.cd73fa2f.js"
+  },
+  {
+    "revision": "1d4bf208c01aae36dcbb2596282a1aef",
+    "url": "/pomodoro-clock/static/media/alarm.1d4bf208.mp3"
+  }
+]);
